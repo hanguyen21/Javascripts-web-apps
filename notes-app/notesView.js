cc57af9b582d53b67b4ae0b95fd280e164596f41
hanguyen <lotusviet.uk@gmail.com>
2023-06-01 12:46:44 +0100

    fixing the notes refresh

--- notes-app/notesView.js
+++ notes-app/notesView.js
@@ -4,16 +4,33 @@ class NotesView {
     constructor(model) {
       this.model = model;
       this.mainContainerEl = document.querySelector('#main-container');
-    }
     
+      this.buttonEl = document.querySelector('#add-note-button');
+
+      document.querySelector('#add-note-button').addEventListener('click', () => {
+        const newNote = document.querySelector('#add-note-input').value;
+        this.addNewNote(newNote);
+      });
+
+    };
+
+    addNewNote(newNote) {
+      this.model.addNote(newNote);
+      
+      this.displayNotes();
+    }
+
     displayNotes() {
+     //  Remove all previous notes
+      document.querySelectorAll('div.note').forEach(e => e.remove());
       const notes = this.model.getNotes()
-  
+      console.log(notes)
       // For each note, create and append a new element on the main container
       notes.forEach(note => {
         const noteEl = document.createElement('div');
         noteEl.textContent = note;
         noteEl.className = 'note';
+        document.querySelector('#add-note-input').value= '';
         this.mainContainerEl.append(noteEl);
       })
     }
--- notes-app/notesView.test.js
+++ notes-app/notesView.test.js
@@ -23,4 +23,46 @@ describe('Notes view', () => {
       // 3. There should now be 2 div.note on the page
       expect(document.querySelectorAll('div.note').length).toEqual(2);
     });
+
+    it('adds a new note',() => {
+      // Arrange
+      document.body.innerHTML = fs.readFileSync('./index.html');
+      const model = new NotesModel();
+      const view = new NotesView(model);
+      
+      
+      // Act
+      const input = document.querySelector('#add-note-input');
+      input.value = 'My new amazing test note';
+
+
+      const button = document.querySelector('#add-note-button');
+      button.click();
+      // Assert
+      expect(document.querySelectorAll('div.note').length).toEqual(1);
+      expect(document.querySelectorAll('div.note')[0].textContent).toEqual('My new amazing test note');
+  
+    });
+    
+    it('clear the list of previous notes before displaying',() => {
+      // Arrange
+      document.body.innerHTML = fs.readFileSync('./index.html');
+      const model = new NotesModel();
+      const view = new NotesView(model);
+      
+    
+      // Act
+      
+      model.addNote('one');
+      model.addNote('two');
+    
+      view.displayNotes();
+      view.displayNotes();
+      
+
+      // Assert
+      expect(document.querySelectorAll('div.note').length).toEqual(2);
+      
+  
+    });
   });
