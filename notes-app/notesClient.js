f40508f2861ca9a3683573392eaeeeed15ab8d05
hanguyen <lotusviet.uk@gmail.com>
2023-06-02 11:31:04 +0100

    test-driving a client class

---
+++ notes-app/notesClient.js
@@ -0,0 +1,15 @@
+class NotesClient {
+
+
+    loadNotes(callback){
+       fetch('http://localhost:3000/notes')
+       .then(response => response.json())
+       .then(data => {
+         callback(data);
+         console.log(data);
+       });
+   }
+    }
+
+
+module.exports = NotesClient;
